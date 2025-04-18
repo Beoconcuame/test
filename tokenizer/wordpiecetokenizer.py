ef757f33326416897b96486482aea8ae78268990
Nguyễn Thái Nhật An <166610549+Beoconcuame@users.noreply.github.com>
2025-04-19 02:09:24 +0700

    Update wordpiecetokenizer.py

--- tokenizer/wordpiecetokenizer.py
+++ tokenizer/wordpiecetokenizer.py
@@ -11,7 +11,8 @@ class WordPieceTokenizer:
         self.tokenizer.train_from_iterator(train_sentences, trainer=trainer)
         self.vocab = self.tokenizer.get_vocab()
         self.pad_id = self.vocab["<PAD>"]
-
+        self.unk_id = self.vocab["<UNK>"]
+        self.vocab_size = vocab_size
     def tokenize_text(self, text, seq_length=None):
         tokens = self.tokenizer.encode(text).ids
         if seq_length is not None:
@@ -25,3 +26,5 @@ class WordPieceTokenizer:
 
     def __call__(self, text, seq_length=None):
         return self.tokenize_text(text, seq_length)
+    def get_vocab_size(self):
+        return self.vocab_size
